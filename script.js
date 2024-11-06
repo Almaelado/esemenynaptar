611102c62dde601db7cdcd274b0d76788e6e3a29
Almaelado <borsny75@gmail.com>
2024-11-06 08:28:04 +0100

    Ütem3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let naphely=document.getElementById("napok");
+let naphely=document.getElementById("honapnap");
 let napok=["H","K","Sz","Cs","P","Sz","V"]
 for(let i=0;i<7;i++){
     let jelen=document.createElement("span");
@@ -6,6 +6,37 @@ for(let i=0;i<7;i++){
     naphely.appendChild(jelen);
 }
 
-function napok(){
+let honapev=document.getElementById("datum");
+let datumok=document.getElementById("napok");
+let mostani=new Date();
+function renderCalendar() {
+    const elsohonap = new Date(mostani.getFullYear(), mostani.getMonth(), 1);
+    const utolsonap = new Date(mostani.getFullYear(), mostani.getMonth() + 1, 0);
     
+    honapev.innerText = `${mostani.toLocaleString('hu-HU', { month: 'long' })} ${mostani.getFullYear()}`;
+
+    const honapok = utolsonap.getDate();
+    const napoko = elsohonap.getDay();
+    
+    datumok.innerHTML = '';
+
+    
+    for (let i = 0; i < napoko; i++) {
+        const div = document.createElement('div');
+        datumok.appendChild(div);
+    }
+
+    
+    for (let i = 1; i <= honapok; i++) {
+        const div2 = document.createElement('div');
+        div2.textContent = i;
+        div2.addEventListener('click', () => openEventModal(i));
+        datumok.appendChild(div2);
+    }
 }
+
+
+
+
+
+renderCalendar();
